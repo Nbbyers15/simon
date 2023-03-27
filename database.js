22c8c0d9b90390c0a449a5a28e74c09fb5501ad5
Nbbyers15 <57930194+Nbbyers15@users.noreply.github.com>
2023-03-27 16:41:55 -0600

    adding mongo database

---
+++ database.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+
+// Read the credentials from environment variables so that you do not accidentally check in your credentials
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAMESIMON;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const scoreCollection = client.db('simon').collection('score');
+
+function addScore(score) {
+  scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = {score: {$gt: 0}};
+  const options = {
+    sort: {score: -1},
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {addScore, getHighScores};
